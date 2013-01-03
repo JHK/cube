e32be67f4e7ea1efe493b71fa1d5d70d280c019a
Christopher Mooney <chris@dod.net>
2013-01-03 13:55:16 -0800

    Added multi-processor support for cube servers
    If options.workers is absent, this code should fork a worker for every
    CPU on the host.  Obviously options.workers may be used to override
    this default.  Please update wiki documentation accordingly.

--- bin/collector.js
+++ bin/collector.js
@@ -1,9 +1,23 @@
-var options = require("./collector-config"),
+var cluster = require('cluster'),
+    options = require("./collector-config"),
     cube = require("../"),
     server = cube.server(options);
 
-server.register = function(db, endpoints) {
-  cube.collector.register(db, endpoints);
-};
+var numCPUs = options.workers || require('os').cpus().length;
 
-server.start();
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+    cluster.fork();
+  });
+} else {
+  server.register = function(db, endpoints) {
+    cube.collector.register(db, endpoints);
+  };
+  
+  server.start();
+}
--- bin/evaluator.js
+++ bin/evaluator.js
@@ -1,9 +1,23 @@
-var options = require("./evaluator-config"),
+var cluster = require('cluster'),
+    options = require("./evaluator-config"),
     cube = require("../"),
     server = cube.server(options);
 
-server.register = function(db, endpoints) {
-  cube.evaluator.register(db, endpoints);
-};
+var numCPUs = options.workers || require('os').cpus().length;
 
-server.start();
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+    cluster.fork();
+  });
+} else {
+  server.register = function(db, endpoints) {
+    cube.evaluator.register(db, endpoints);
+  };
+
+  server.start();
+}
